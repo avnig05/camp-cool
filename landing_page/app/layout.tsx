614f5ac3aa5014de338e4b0b527fba11a7655f4e
Nxver-GitHub <suryapugaz1629@gmail.com>
2025-05-16 02:33:06 -0700

    Fixed hydration issue regarding web extensions

--- landing_page/app/layout.tsx
+++ landing_page/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider 
           attribute="class" 
           defaultTheme="light" 
